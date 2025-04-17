f7872455e15999a607f63d8db8c32af03b1e5a37
Aniket Kumar <aniketsinha101@gmail.com>
2025-04-17 15:37:28 +0530

    Longest Common update

--- Dynamic Programming/LongestCommonSubsequence.cpp
+++ Dynamic Programming/LongestCommonSubsequence.cpp
@@ -74,9 +74,13 @@ class DPSolution {
 public:
     int longestCommonSubsequence(const string& text1, const string& text2) {
         int n = text1.size(), m = text2.size();
+        // reason for n+1 and m+1 is to handle the base case of empty string
+        // dp[i][j] will hold the length of LCS of text1[0..i-1] and text2[0..j-1]
         vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
         for (int i = 1; i <= n; i++) {
+            //we need to iterate from 1 to n and 1 to m because we are using 1-based indexing for dp array
             for (int j = 1; j <= m; j++) {
+                //the reason we dont compare from i=0 and j=0 is because we are using 1-based indexing for dp array
                 if (text1[i - 1] == text2[j - 1])
                     dp[i][j] = 1 + dp[i - 1][j - 1];
                 else
