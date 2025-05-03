881cfe76d33492df4a39cdb60712a475a7c0081a
Aniket Kumar <aniketsinha101@gmail.com>
2025-05-03 18:08:21 +0530

    Update mDesk.cpp

--- Array/TwoPointer/mDesk.cpp
+++ Array/TwoPointer/mDesk.cpp
@@ -1,4 +1,5 @@
 You need to buy monitors for m desks. Each desk can have at most 2 monitors. You’re given a list of monitor prices, and for each desk, the total price of monitors should not exceed a given budget X. Find out how many monitors you can buy.
+We want to maximize the monitor assigned, hence everytime the pair does not exist, it is more wise to assign a monitor from the top of the vector.
 
 def max_monitors(arr, m, X):
     arr.sort()
@@ -8,13 +9,12 @@ def max_monitors(arr, m, X):
 
     while i <= j and desks_used < m:
         if arr[i] + arr[j] <= X:
-            # Pair i and j
             monitors_assigned += 2
             i += 1
             j -= 1
         else:
-            # Use only j
-            monitors_assigned += 1
+            if arr[j] <= X:
+                monitors_assigned += 1
             j -= 1
         desks_used += 1
 
