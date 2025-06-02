02e8ee79166e4b5c67b15d092856914c52a04a4a
Aniket Kumar <aniketsinha101@gmail.com>
2025-06-02 14:22:23 +0530

    Update GoogleSolar.cpp

--- UnorderedMap/GoogleSolar.cpp
+++ UnorderedMap/GoogleSolar.cpp
@@ -2,22 +2,18 @@
 #include <unordered_map>
 #include <deque>
 #include <string>
-#include <functional> // for std::hash
 
 using namespace std;
 
 class Logger {
 private:
     const int WINDOW_SIZE = 10;
-
     deque<pair<int, string>> window;
-    unordered_map<size_t, deque<pair<int, string>>::iterator> msgMap;
-    hash<string> hasher;
+    unordered_map<string, deque<pair<int,string>>::iterator> msgMap;
 
     void evictOldMessages(int currentTimestamp) {
         while (!window.empty() && window.front().first <= currentTimestamp - WINDOW_SIZE) {
-            size_t oldHash = hasher(window.front().second);
-            msgMap.erase(oldHash);
+            msgMap.erase(window.front().second);
             window.pop_front();
         }
     }
@@ -26,19 +22,41 @@ public:
     bool shouldPrint(int timestamp, const string& message) {
         evictOldMessages(timestamp);
 
-        size_t h = hasher(message);
-        auto it = msgMap.find(h);
-        if (it != msgMap.end()) {
-            // Possible duplicate - verify strings to avoid hash collision issues
-            if (it->second->second == message) {
-                // Duplicate within window - suppress print
-                return false;
-            }
+        if (msgMap.find(message) != msgMap.end()) {
+            // Message seen within 10 seconds; do not print
+            return false;
         }
 
+        // Insert new message at back and store iterator in map
         window.emplace_back(timestamp, message);
-        auto dequeIt = prev(window.end());
-        msgMap[h] = dequeIt;
+        auto it = prev(window.end());
+        msgMap[message] = it;
+
         return true;
     }
 };
+
+int main() {
+    vector<string> inputs = {
+        "10 solar panel activated",
+        "11 low battery warning",
+        "12 tire one: low air pressure",
+        "13 solar panel activated",
+        "14 low battery warning",
+        "21 solar panel activated",
+        "35 solar panel activated"
+    };
+
+    Logger logger;
+
+    for (const auto& input : inputs) {
+        size_t pos = input.find_first_of(" \t\n\r");
+        int timestamp = stoi(input.substr(0, pos));
+        string message = input.substr(pos + 1);
+
+        if (logger.shouldPrint(timestamp, message)) {
+            cout << timestamp << " " << message << endl;
+        }
+    }
+    return 0;
+}
