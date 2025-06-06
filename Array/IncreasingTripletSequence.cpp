f8106abfff59feb5ef6883fd159ff734781a20eb
Aniket Kumar <aniketsinha101@gmail.com>
2025-06-06 19:24:53 +0530

    Create IncreasingTripletSequence.cpp

---
+++ Array/IncreasingTripletSequence.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+               int first = INT_MAX, second = INT_MAX;
+
+        for (int num : nums) {
+            if (num <= first) {
+                first = num;            // smallest so far
+            } else if (num <= second) {
+                second = num;           // second smallest so far
+            } else {
+                return true;            // found third element > second
+            }
+        }
+
+        return false;
+    }
+};
