37c4d7b4ee4880a2f6d16047ded764547bfeb527
Aniket Kumar <aniketsinha101@gmail.com>
2025-06-11 13:05:02 +0530

    Update Regex.cpp

--- Dynamic Programming/Regex.cpp
+++ Dynamic Programming/Regex.cpp
@@ -24,9 +24,15 @@ public:
                     M[i][j] = M[i - 1][j - 1];
                 } else if (curP == '*') {
                     char preCurP = p[j - 2];
+                    //If:The previous pattern character (preCurP) is not ., and
+                    //It does not match the current string character (curS),
+                    //Then:
+                    //There's no way the * can match the current string character.
+                    //So your only option is to ignore the 'preCurP*' (treat it as matching zero occurrences).
                     if (preCurP != '.' && preCurP != curS) {
                         M[i][j] = M[i][j - 2];
                     } else {
+                        //“If I match the current s[i - 1] with 'char*', then can the previous part of s match the same pattern?”
                         M[i][j] = M[i][j - 2] ||  M[i - 1][j];
                     }
                 }
