aaa5b43e0ebc7918f74316313c798a5cb7ff8de7
Aniket Kumar <aniketsinha101@gmail.com>
2025-06-12 14:13:03 +0530

    Update Mistakes.cpp

--- Mistakes.cpp
+++ Mistakes.cpp
@@ -6,3 +6,8 @@ ListNode* tail = &dummy; // pointer
 
 dummy.next = new ListNode(5);   // dot — actual object
 tail->next = new ListNode(6);   // arrow — pointer
+
+
+-> Order of the execution statement matter, although logically to our brain, it looks good, but deep within it might be mesed.
+executing statement A, before B
+and changing the order can have drastic effect on the statement
