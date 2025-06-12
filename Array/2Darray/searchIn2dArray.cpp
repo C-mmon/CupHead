439cb63497d3d6904ff77c81977694ff736c1331
Aniket Kumar <aniketsinha101@gmail.com>
2025-06-12 19:44:10 +0530

    Update searchIn2dArray.cpp

--- Array/2Darray/searchIn2dArray.cpp
+++ Array/2Darray/searchIn2dArray.cpp
@@ -1,24 +1,26 @@
-//Perform a normal binary search
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        if(matrix.empty() || matrix[0].empty()) return false;
         
-        int n = matrix.size();
-        int m = matrix[0].size();
-        int s = 0, e = n * m - 1;
-        
-        while(s <= e){
-            int mid = (s + e) / 2;
-            int row = mid / m;
-            int col = mid % m;
-            
-            if(matrix[row][col] == target){
+        int row = matrix.size();
+        int col = matrix[0].size();
+        int low = 0;
+        int high = matrix.size() * matrix[0].size();
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int newRow = mid / row ;
+            int newColumn= mid % col;
+            int middleElement = matrix[newRow][newColumn];
+            if (middleElement == target) {
                 return true;
-            } else if(matrix[row][col] < target) {
-                s = mid + 1;
-            } else {
-                e = mid - 1;
+            }
+            else if(middleElement <  target)
+            {
+                low = mid+1;
+            }
+            else
+            {
+                high =  mid-1;
             }
         }
         return false;
