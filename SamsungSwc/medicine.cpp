34a5f2632496fa2479946c458d88f4b0c1ee4856
Aniket Kumar <aniketsinha101@gmail.com>
2025-07-25 03:50:19 +0530

    Create medicine.cpp

---
+++ SamsungSwc/medicine.cpp
@@ -0,0 +1,14 @@
+We have a truck, M medicines, and N containers. Given an array container_capacity of length N, we need to transport all the medicines from one place to another using a single truck.
+
+Constraints:
+The truck can carry a maximum of 2 containers at a time.
+
+A container must be fully filled before transport; partial filling is not allowed.
+
+truck can carry single container.
+
+The goal is to deliver all medicines in the minimum number of trips.
+
+if its not possible return -1.
+
+i pass all sample cases but result is pending.
