dd66e973c194564b6b2fc6e76dc31f67c61d91e8
Aniket Kumar <aniketsinha101@gmail.com>
2025-07-25 03:55:56 +0530

    Create necklace.cpp

---
+++ SamsungSwc/necklace.cpp
@@ -0,0 +1,7 @@
+I just wanted to ask did you any of u guys appeared for it and if yes the problem was a (NECKLACE) problem with red and blue beads if i remember correctly , so the question is despite of my code being compiled correctly and giving the correct answers for all the 6 non-hidden test cases ,but when i submitted it was my (4/10) fourth out of 10 try , so i asked the proctor that i have submitted the code what should i do , then after a few minutes she gave me an asnwer that u have failed the test.
+
+When i asked the reason she stated its an automated process and here it is showing failed , so now u can leave the meeting and end the test (P.S i had few tries and half an hour left).
+
+So chat am i cooked, or what is the criteria that non hidden test cases all pass then it shows paas to the proctor. She didn't even told me that i could attempt again i thought that code is submitted means its has paased all the test cases.
+
+So anybody can enlighten me on this issue why it happened or whats the case despite being code submitted it told failed may be because of hidden test cases or some other reason???????
