9687c1cb364d8f92487f80903efdbced9b7c9064
Aniket Kumar <aniketsinha101@gmail.com>
2025-08-31 23:53:09 +0530

    Create Amazon.cpp

---
+++ Amazon.cpp
@@ -0,0 +1,5 @@
+
+
+=> File fitering and search system . Ask was to how I would solve it.
+I answered like to use decorator and factory and stratgey pattern . Obviously gave red flag , because interviewer was expecting like how would you take in query parameters . Told to use maybe class models for that , I didnt understand anything and had no idea how to do that. Got into union and intersection of queries .
+
